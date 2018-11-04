90e2f3ff594137d67eda6dadc84a37947ce872fe
Haris Hussain <hussainharis04@gatech.edu>
2018-11-04 00:13:48 -0400

    First commit

---
+++ geoLocation/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  geoLocation
+//
+//  Created by Haris Hussain on 11/3/18.
+//  Copyright © 2018 Silly. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: 33.7756, longitude: -84.3963, zoom: 16.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+
+
+}
+
